881038dba680540f45bcc371ac76846196a96982
Alex Walton <alex.w.walton@gmail.com>
2018-11-24 17:36:54 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Elm Counter
+
+A simple counter written in Elm.
+
+## Getting started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Installing development dependencies
+
+```
+npm install
+```
+
+
+### Running
+
+To watch the `src/` directory for changes and recompile the application with the Time Travelling Debugger:
+
+```
+npm start
+```
+
+This will serve the application locally at `http://localhost:8080`
+
+
+### Production build
+
+Compiles the Elm code with the `--optimize` flag:
+
+```
+npm run build:production
+```
+
+To serve the optimized build:
+
+```
+npm run serve
+```
+
+This will serve the application locally at `http://localhost:8080`
---
+++ src/Todo.elm
@@ -0,0 +1,135 @@
+import Browser
+import Html exposing (Html, button, div, span, text, input, h1, ul, li, label)
+import Html.Events exposing (onClick, onInput)
+import Html.Attributes exposing (checked, style, placeholder, value, type_)
+
+-- MAIN
+
+main =
+  Browser.element 
+    { init = init
+    , subscriptions = subscriptions
+    , update = update
+    , view = view
+    }
+
+
+-- MODEL
+
+type TodoStatus = Complete | Incomplete
+
+not : TodoStatus -> TodoStatus
+not status =
+  case status of
+    Complete -> Incomplete
+    Incomplete -> Complete
+
+type alias Todo =
+  { id : Int
+  , label : String
+  , status : TodoStatus
+  }
+
+type alias Model =
+  { todos : List Todo
+  , newTodo : String
+  }
+
+init : () -> (Model, Cmd Msg)
+init _ =
+  ({ todos = [], newTodo = "" }
+  , Cmd.none
+  )
+
+
+-- UPDATE
+
+-- Incremental IDs for now. Depends on Todos being prepended to list.
+nextId : List Todo -> Int
+nextId todos =
+  case List.head todos of
+    Just todo ->
+      todo.id + 1
+    
+    Nothing ->
+      1
+
+type Msg 
+  = UpdateNewTodo String
+  | ToggleTodo Int
+  | CreateTodo
+
+toggleTodo : List Todo -> Int -> List Todo
+toggleTodo list idToToggle =
+  let
+    toggle : Todo -> Todo
+    toggle todo =
+      if todo.id == idToToggle then
+        { todo | status = not todo.status }
+      else
+        todo
+  in
+    List.map toggle list
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    UpdateNewTodo str ->
+      ({ model | newTodo = str }, Cmd.none)
+    
+    CreateTodo ->
+      let
+        newTodo = ""
+        todo =
+          { id = nextId model.todos
+          , label = model.newTodo
+          , status = Incomplete }
+      in
+      ({ model | newTodo = newTodo, todos = todo :: model.todos }, Cmd.none)
+
+    ToggleTodo id ->
+      let
+        todos = toggleTodo model.todos id
+      in
+      ({ model | todos = todos }, Cmd.none)
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+-- VIEW
+
+numberStyle : Int -> List (Html.Attribute msg)
+numberStyle n =
+  [ style "width" "5em"
+  , style "display" "inline-block"
+  , style "text-align" "center"
+  ]
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text "Todo?" ]
+    , input [ placeholder "What do you need to do?", value model.newTodo, onInput UpdateNewTodo ] []
+    , button [ onClick CreateTodo ] [ text "Create" ]
+    , ul [] (List.map renderTodo model.todos)
+    ]
+
+statusToBool : TodoStatus -> Bool
+statusToBool status =
+  case status of
+    Complete -> True
+    Incomplete -> False
+
+renderTodo : Todo -> Html Msg
+renderTodo todo =
+  li []
+    [ label []
+      [ input [ type_ "checkbox", statusToBool todo.status |> checked, onClick (ToggleTodo todo.id)  ] []
+      , span [] [ text todo.label ]
+      ]
+    ]
